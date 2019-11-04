86172dd49adf9f9547e3b98e2e308648d7a32afc
kazeaisu <matheushiki7@gmail.com>
2019-11-04 18:38:45 +0900

    aa

--- yorosiku.js
+++ yorosiku.js
@@ -1,12 +1,22 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-var channel = ""
+const fs = require('fs');
+var channel = "617577875574620160"
+
+let image = [];
+fs.readdir('./gif file', function(err, files){
+	if(err) throw new Error()
+	for(var i = 0; i < files.length; i++){
+		console.log(files)
+	}
+});
+
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
 client.on("guildMemberAdd" , async member => {
-client.channels.get("")
+	client.channels.get("557151054723022859").send()
 
 });
 
