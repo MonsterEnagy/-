d3795b2e36e8cb0fc06075cc3143e4f7365f9a77
kazeaisu <matheushiki7@gmail.com>
2019-11-04 18:27:54 +0900

    aa

--- yorosiku.js
+++ yorosiku.js
@@ -1,12 +1,12 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-
+var channel = ""
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
 client.on("guildMemberAdd" , async member => {
-
+client.channels.get("")
 
 });
 
